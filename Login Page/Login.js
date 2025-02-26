f883b085cbac31dd9ecdb9afbb45e86bf2222093
chethan <87270631+ChethanV2012@users.noreply.github.com>
2025-02-26 18:47:04 +0530

    hi
    upload

---
+++ Login Page/Login.js
@@ -0,0 +1,31 @@
+// script.js
+
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get Form Values
+    var email = document.getElementById("email").value.trim();
+    var password = document.getElementById("password").value.trim();
+
+    let isValid = true;
+
+    // Validation
+    if (!email) {
+        alert("Email Id is required.");
+        isValid = false;
+    }
+    if (!password) {
+        alert("Password is required.");
+        isValid = false;
+    }
+
+    if (!isValid) return;
+
+    // Example validation for credentials
+    if (email === "admin@example.com" && password === "password123") {
+        alert("Login successful!");
+        window.location.href = "C:\colleg\Incognito _main\Admin_Dashboard\Admin.html"; // Redirect to the dashboard page
+    } else {
+        alert("Invalid Email Id or Password.");
+    }
+});
