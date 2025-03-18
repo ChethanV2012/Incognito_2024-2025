039a47d6dbf17de9f47dfd70be246733168f2279
chethan <87270631+ChethanV2012@users.noreply.github.com>
2025-03-18 21:04:08 +0530

    changed

--- Events Page/event.js
+++ Events Page/event.js
@@ -1,132 +1,132 @@
 const events = {
-  
-    technical: [
-      {
-        name: "IT DUMCHARADAS",
-        image: "https://rupamkerketta.github.io/techleons-2020/resources/images/logos/events/dumb-charades-min.png",
-        details: "Test your coding skills in this 2-hour challenge.",
-        rules: [
-          "Team Event, 3 members per team.",
-          "There will be 3 Rounds.",
-          "Only Usage of gestures, facial expressions, and body movements to convey the phrase is allowed",
-          "Use of mobile phones or any other electronic gadgets is strictly prohibited.",
-          "If anybody is found cheating, will be disqualified.",
-          "Judges decision is final.",
-          "Further rules will be appended on the spot.",
-        ],
-        facultyCoordinators: "NETHRA N",
-      studentCoordinators: [
-        "PRAJWAL S:7349107106 \n",
-        "SUSHEENDRA ",
-      ],
-        registerLink: "https://example.com/register/technical1"
-      },
 
-      {
-        name: "IT QUIZ (QUIZZE BRAINAX)",
-        image: "https://www.pngmart.com/files/19/Quiz-Logo-PNG-Photos-407x279.png",
-        
-        rules: [
-          " Computer Science students are eligible to participate.",
-          " 2 members in a team.",
-          " The event has 3 rounds.",
-          " Teams must provide answers within the allocated time.",
-          " Judges' decision is final.",
-          " Disruptive behavior lead to disqualification.",
-          " Electronic gadgets are strictly prohibited.",
-          " Further rules will be appended on the spot."
-        ],
-        facultyCoordinators: "BHARTHI N",
-      studentCoordinators: [
-        "TARUN :6361741971",
-        "VANI V",
+  technical: [
+    {
+      name: "IT DUMCHARADAS",
+      image: "https://rupamkerketta.github.io/techleons-2020/resources/images/logos/events/dumb-charades-min.png",
+      details: "Test your coding skills in this 2-hour challenge.",
+      rules: [
+        "Team Event, 3 members per team.",
+        "There will be 3 Rounds.",
+        "Only Usage of gestures, facial expressions, and body movements to convey the phrase is allowed",
+        "Use of mobile phones or any other electronic gadgets is strictly prohibited.",
+        "If anybody is found cheating, will be disqualified.",
+        "Judges decision is final.",
+        "Further rules will be appended on the spot.",
       ],
-        registerLink: "https://example.com/register/technical1"
-      },
-      {
-        name: "IOT MODEL (TECH SPARK)",
-        image: "https://cdn3d.iconscout.com/3d/premium/thumb/iot-11102654-8884752.png",
-        details: "Test your coding skills in this 2-hour challenge.",
-        rules: [
-          "Teams consist of 1-4 members.",
-          "Teams should build and test their IOT projects earlier.",
-          "Projects are evaluated based on criteria like functionality, user-friendliness, and scalability.",
-          "Teams may need to provide a detailed written description, pseudocode, diagrams, explanation, and a demonstration using chart.",
-          "Participants are required to supply their own tools.",
-          "Judges' decision is final.",
-          "Further rules will be appended on the spot.",
-        ],
-        facultyCoordinators: "SHALINI M K",
-        studentCoordinators: [
-          "YUVARAJ C: 7204709502",
-          "SHRUTHI K G",
-        ],
-        registerLink: "https://example.com/register/technical1"
-      },
-      {
-        name: "CODING & DEBUGGING (CODE CRAFT)",
-        image: "https://static-00.iconduck.com/assets.00/coding-illustration-1024x682-mkaob758.png",
-        details: "Test your coding skills in this 2-hour challenge.",
-        rules: [
-          "Team Event, 2 members per team.",
-          "Use of mobile phone or any electronic gadgets are strictly prohibited",
-          "If anybody is found cheating, will be disqualified.",
-          "There will be 3 rounds.",
-          "Judges decision is final.",
-          "First round MCQ based code snippet",
-          "Other 2 rounds will be based on Debugging & Coding",
-          "C/C++, Java, Python can be used.",
-          "Further rules will be appended on the spot",
-        ],
-        facultyCoordinators: "VISMAYA P K",
-        studentCoordinators: [
-          "HIMATEJU V:6366145580",
-          "RENUKA",
-        ],
-        registerLink: "https://example.com/register/technical1"
-      },
-      {
-        name: "IT TECHMASTER (THE IT TRIFECTA)",
-        image: "https://www.techmaster.my/wp-content/uploads/2020/08/logo-techmaster.png",
-        details: "Test your coding skills in this 2-hour challenge.",
-        rules: [
-          "Individual Event.",
-          "The event has a total of 3 rounds.",
-          "Use of mobile phones or any other electronic gadgets are strictly prohibited.",
-          "If anybody is found cheating, will be disqualified.",
-          " Judges' decision is final.",
-          "Further rules will be appended on the spot."
-        ],
-        facultyCoordinators: "DEEKSHA HOLLA",
-        studentCoordinators: [
-          "SUNIL KUMAR J.:8867479314",
-          "SUPREETH G R ",
-        ],
-        registerLink: "https://example.com/register/technical1"
-      },
-      {
-        name: "WEB DEVELOPEMENT (WEB VERSE)",
-        image: "https://cdni.iconscout.com/illustration/premium/thumb/web-development-3817577-3181531.png",
-        details: "Test your coding skills in this 2-hour challenge.",
-        rules: [
-          "Each team can have a maximum of 2 members.",
-          "The competition will consist of up to 2-3 different rounds.",
-          "Participants must bring their own laptops along with ahy other necessary accessories.",
-          "Only traditional IDEs like VS Code, Notepad++ etc. are permitted.",
-          "The use of Al-powered tools such as Cursor, ChatGPT, Copilot, or similar is strictly prohibited.",
-          "* Only HTML, CSS, and JavaScript are allowed. Frameworks and libraries are not permitted.",
-          
-        ],
-        facultyCoordinators: "SOWMYA K",
-        studentCoordinators: [
-          "KIRAN KUMAR G.:9019709984",
-          "HARSHITH ",
-          
-        ],
-        registerLink: "https://example.com/register/technical1"
-      },
-      
-    ],
+      facultyCoordinators: "NETHRA N",
+      studentCoordinators: [
+        "SUSHEENDRA ",
+        "PRAJWAL S:7349107106 \n",
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+
+    {
+      name: "IT QUIZ (QUIZZE BRAINAX)",
+      image: "https://www.pngmart.com/files/19/Quiz-Logo-PNG-Photos-407x279.png",
+
+      rules: [
+        " Computer Science students are eligible to participate.",
+        " 2 members in a team.",
+        " The event has 3 rounds.",
+        " Teams must provide answers within the allocated time.",
+        " Judges' decision is final.",
+        " Disruptive behavior lead to disqualification.",
+        " Electronic gadgets are strictly prohibited.",
+        " Further rules will be appended on the spot."
+      ],
+      facultyCoordinators: "BHARTHI N",
+      studentCoordinators: [
+        "VANI V",
+        "TARUN :6361741971",
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+    {
+      name: "IOT MODEL (TECH SPARK)",
+      image: "https://cdn3d.iconscout.com/3d/premium/thumb/iot-11102654-8884752.png",
+      details: "Test your coding skills in this 2-hour challenge.",
+      rules: [
+        "Teams consist of 1-4 members.",
+        "Teams should build and test their IOT projects earlier.",
+        "Projects are evaluated based on criteria like functionality, user-friendliness, and scalability.",
+        "Teams may need to provide a detailed written description, pseudocode, diagrams, explanation, and a demonstration using chart.",
+        "Participants are required to supply their own tools.",
+        "Judges' decision is final.",
+        "Further rules will be appended on the spot.",
+      ],
+      facultyCoordinators: "SHALINI M K",
+      studentCoordinators: [
+        "SHRUTHI K G",
+        "YUVARAJ C: 7204709502",
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+    {
+      name: "CODING & DEBUGGING (CODE CRAFT)",
+      image: "https://static-00.iconduck.com/assets.00/coding-illustration-1024x682-mkaob758.png",
+      details: "Test your coding skills in this 2-hour challenge.",
+      rules: [
+        "Team Event, 2 members per team.",
+        "Use of mobile phone or any electronic gadgets are strictly prohibited",
+        "If anybody is found cheating, will be disqualified.",
+        "There will be 3 rounds.",
+        "Judges decision is final.",
+        "First round MCQ based code snippet",
+        "Other 2 rounds will be based on Debugging & Coding",
+        "C/C++, Java, Python can be used.",
+        "Further rules will be appended on the spot",
+      ],
+      facultyCoordinators: "VISMAYA P K",
+      studentCoordinators: [
+        "RENUKA",
+        "HIMATEJU V:6366145580",
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+    {
+      name: "IT TECHMASTER (THE IT TRIFECTA)",
+      image: "https://www.techmaster.my/wp-content/uploads/2020/08/logo-techmaster.png",
+      details: "Test your coding skills in this 2-hour challenge.",
+      rules: [
+        "Individual Event.",
+        "The event has a total of 3 rounds.",
+        "Use of mobile phones or any other electronic gadgets are strictly prohibited.",
+        "If anybody is found cheating, will be disqualified.",
+        " Judges' decision is final.",
+        "Further rules will be appended on the spot."
+      ],
+      facultyCoordinators: "DEEKSHA HOLLA",
+      studentCoordinators: [
+        "SUPREETH G R ",
+        "SUNIL KUMAR J.:8867479314",
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+    {
+      name: "WEB DEVELOPEMENT (WEB VERSE)",
+      image: "https://cdni.iconscout.com/illustration/premium/thumb/web-development-3817577-3181531.png",
+      details: "Test your coding skills in this 2-hour challenge.",
+      rules: [
+        "Each team can have a maximum of 2 members.",
+        "The competition will consist of up to 2-3 different rounds.",
+        "Participants must bring their own laptops along with ahy other necessary accessories.",
+        "Only traditional IDEs like VS Code, Notepad++ etc. are permitted.",
+        "The use of Al-powered tools such as Cursor, ChatGPT, Copilot, or similar is strictly prohibited.",
+        "* Only HTML, CSS, and JavaScript are allowed. Frameworks and libraries are not permitted.",
+
+      ],
+      facultyCoordinators: "SOWMYA K",
+      studentCoordinators: [
+        "HARSHITH ",
+        "KIRAN KUMAR G.:9019709984",
+
+      ],
+      registerLink: "https://example.com/register/technical1"
+    },
+
+  ],
 
   "non-technical": [
     {
@@ -139,12 +139,12 @@ const events = {
         "The number of participants Solo (1 Candidate), Group(Minimum 2, Maximum 10 Candidates).",
         "Time Limit: 4+2 Minutes for Solo, 8+2 Minutes for Group.",
         "Maximum of 3 instruments allowed",
-        
+
       ],
       facultyCoordinators: "SUJATHA P V",
       studentCoordinators: [
-        "DEEPTHI G: 8197121042",
         "PRARTHANA ",
+        "DEEPTHI G: 8197121042",
       ],
       registerLink: "https://example.com/register/technical1"
     },
@@ -157,13 +157,12 @@ const events = {
         "The number of participants Solo(1 Candidate), Group(Minimum 4, Maximum 8 Candidates).",
         "A performance must last for 6 Minutes and may extend to 12 Minutes including setup time.",
         "Exceeding the maximum time limit will result in a negative marking.",
-        
+
       ],
       facultyCoordinators: "SUJATHA P V",
       studentCoordinators: [
-        
-        "PRARTHANA: 9740727045 ",
         "DEEPTHI G ",
+        "PRARTHANA: 9740727045 ",
       ],
       registerLink: "https://example.com/register/technical1"
     },
@@ -180,9 +179,8 @@ const events = {
       ],
       facultyCoordinators: "SUJATHA P V",
       studentCoordinators: [
-       
-        "PRARTHANA: 9740727045 ",
         "DEEPTHI G ",
+        "PRARTHANA: 9740727045 ",
       ],
       registerLink: "https://example.com/register/technical1"
     },
@@ -200,8 +198,8 @@ const events = {
       ],
       facultyCoordinators: "SHIVASWAMY D S",
       studentCoordinators: [
-        "GOVARDHAN K:7337685349",
         "VIGNESH ",
+        "GOVARDHAN K:7337685349",
       ],
       registerLink: "https://example.com/register/technical1"
     },
@@ -215,57 +213,58 @@ const events = {
         "Video must be short and edited on spot by same member",
         "Further rules appended on the spot",
         "Judges' decision is final",
-        
+
       ],
       facultyCoordinators: "SOWMYA B S",
       studentCoordinators: [
-        
-        "CYAN BINOY PHILIP: 9008983019",
         "HEMANTH KUMAR B",
+        "CYAN BINOY PHILIP: 9008983019",
       ],
       registerLink: "https://example.com/register/technical1"
     },
   ],
   "gaming": [
-    { name: "BGMI (CONQUERORS CLASH)", 
+    {
+      name: "BGMI (CONQUERORS CLASH)",
       image: "https://cdn130.picsart.com/366226577087211.png",
-       details: "Battle it out in this intense BGMI tournament.", 
-       rules:[
+      details: "Battle it out in this intense BGMI tournament.",
+      rules: [
         "Team Event, 2 members per team.",
         "There will be 3 matches with no elimination.",
         "In case of disconnection (due to high ping) no rematch will be conducted.",
         "No hacks, Glitches or Modders allowed.",
         "No verbal abusing to other players, No teaming",
         "College will not provide any wifi or Internet."
-       ],
-       facultyCoordinators: "SOWMYA K",
-       studentCoordinators: [
-        "KARTHIK MIRAJKAR: 7975006949",
-        "DRITHI ",
       ],
-       registerLink: "https://example.com/register/technical1",
-      },
-      { name: "FREE FIRE (FIRE OF CLASH)", 
-        image: "https://www.pngmart.com/files/22/Garena-Free-Fire-PNG.png",
-         details: "Battle it out in this intense BGMI tournament.", 
-         rules:[
-          "Team Event, 4 members per team.",
-          "Provide your player's ID during registration.",
-          "There will be 3 matches with no no elimin elimination [BR]",
-          " In case of disconnection (due to high ping) no rematch will be conducted.",
-          "Hacks, Glitches or Panel are strictly prohibited.",
-          "Earpods or earphones mandatory, college will not provide wifi (or) internet.",
-          "* Breaking the rules can result in disqualification.",
-          "Further rules will be shared on the spot."
-         ],
-         facultyCoordinators: "RAVIKUMAR G",
-         studentCoordinators: [
-         
-          "MOKSHITH S: 9620060620",
-          "CHETHAN V ",
-        ],
-         registerLink: "https://example.com/register/technical1",
-        },
+      facultyCoordinators: "SOWMYA K",
+      studentCoordinators: [
+        "DRITHI ",
+        "KARTHIK MIRAJKAR: 7975006949",
+      ],
+      registerLink: "https://example.com/register/technical1",
+    },
+    {
+      name: "FREE FIRE (FIRE OF CLASH)",
+      image: "https://www.pngmart.com/files/22/Garena-Free-Fire-PNG.png",
+      details: "Battle it out in this intense BGMI tournament.",
+      rules: [
+        "Team Event, 4 members per team.",
+        "Provide your player's ID during registration.",
+        "There will be 3 matches with no no elimin elimination [BR]",
+        " In case of disconnection (due to high ping) no rematch will be conducted.",
+        "Hacks, Glitches or Panel are strictly prohibited.",
+        "Earpods or earphones mandatory, college will not provide wifi (or) internet.",
+        "* Breaking the rules can result in disqualification.",
+        "Further rules will be shared on the spot."
+      ],
+      facultyCoordinators: "RAVIKUMAR G",
+      studentCoordinators: [
+        "CHETHAN V: 9845468518 ",
+        "MOKSHITH S",
+
+      ],
+      registerLink: "https://example.com/register/technical1",
+    },
   ]
 };
 
